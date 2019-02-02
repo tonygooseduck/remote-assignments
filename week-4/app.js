16e38c8407203e81d82f9a79abdd5afbc693a593
Charles Tang <tonygooseduck@gmail.com>
2019-02-02 17:21:14 +0800

    assignment4 second commit

--- week-4/app.js
+++ week-4/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const mysqldump = require('mysqldump');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -40,23 +41,60 @@ app.get('/createtable', (req, res) => {
     res.send('Table created...');
   });
 });
-
+// sign in and sign up homepage
 app.get('/', (req, res) => {
   res.render('form');
+
 });
 
 app.post('/signin', (req, res) => {
-  // add sql code
-  res.send(req.body.password);
+  let email = req.body.email;
+  let sql = `SELECT * FROM user WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
+  db.query(sql, (err, result) => {
+    if (err) console.log(err.message);
+    // succeed to sign in
+    if (result.length > 0) {
+        res.redirect('/member');
+    }
+
+    // fail to sign in
+    else res.redirect(404, '/');
+  });
 });
 
 app.post('/signup', (req, res) => {
-  //add sql code
-  res.send('checking whether already signed up before');
+  let sql = `SELECT * FROM user WHERE email = '${req.body.email}'`;
+  db.query(sql, (err, result) => {
+    if (err) console.log(err.message);
+    // able to sign up
+    if (result.length === 0) {
+      let sql = `INSERT INTO user (email, password) VALUES ('${req.body.email}', '${req.body.password}')`;
+      db.query(sql, (err, result) => {
+        if(err) console.log(err.message);
+        if(result.affectedRows === 1) {
+          res.redirect('/member');
+        }
+      });
+
+    }
+    // unable to sign up
+    else res.redirect(404, '/');
+  });
 });
 
+app.get('/member', (req, res) => {
+  res.render('member');
+});
 
-
+mysqldump({
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'AWShw04@0203',
+        database: 'assignment'
+    },
+    dumpToFile: './dump.sql',
+})
 
 app.listen('3000', () => {
   console.log('Server started on port 3000');
